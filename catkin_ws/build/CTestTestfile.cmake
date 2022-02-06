08c211325eba64d4bbb9eda08a460f6f7da4fd1b
Tosin Oseni <43161147+tohsin@users.noreply.github.com>
2022-02-06 13:44:24 +0100

    created catkin work space
    workspace cretaed using robostack conda environment

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/elena/dev/Robotics-Software-Engineer-Udacity/catkin_ws/src
+# Build directory: /Users/elena/dev/Robotics-Software-Engineer-Udacity/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
